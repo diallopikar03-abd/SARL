3e9483d398671ab344c53ef547cc1dbd96751ae0
abdoulaye <quinzambougou360@gmail.com>
2026-09-19 22:11:53 +0200

    correction de db.php

--- db.php
+++ db.php
@@ -1,22 +1,23 @@
 <?php
+
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT');
+$dbname = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+
 try {
-// =====================================================
-// CONNEXION À LA BASE DE DONNÉES
-// Railway OU XAMPP
-// =====================================================
 
-$host = getenv('MYSQLHOST') ?: 'localhost';
-$port = getenv('MYSQLPORT') ?: '3306';
-$dbname = getenv('MYSQLDATABASE') ?: 'Site_web';
-$user = getenv('MYSQLUSER') ?: 'root';
-$password = getenv('MYSQLPASSWORD') ?: '';
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password
+    );
 
-  // Connexion forcée en TCP/IP avec les accès en clair
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
-} catch(Exception $e) { 
-    die('Erreur : ' . $e->getMessage()); 
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+
+    die("Erreur : " . $e->getMessage());
 }
-?>
