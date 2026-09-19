ffb82e414c6baa4a27ac16ca170ee2726a8d1e25
abdoulaye <quinzambougou360@gmail.com>
2026-09-19 20:00:55 +0200

    modification du fichier db.php

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
 try {
-    // Récupération des informations de Railway (ou valeurs par défaut en local)
-    $host = getenv('MYSQLHOST') ?: 'localhost';
-    $port = getenv('MYSQLPORT') ?: '3306';
-    $dbname = getenv('MYSQLDATABASE') ?: 'Site_web';
-    $user = getenv('MYSQLUSER') ?: 'root';
-    $password = getenv('MYSQLPASSWORD') ?: '';
+    // REMPLACEZ LES TEXTES CI-DESSOUS PAR VOS INFOS DE L'ONGLET VARIABLES DE RAILWAY
+    $host = 'METTRE_ICI_LA_VALEUR_DE_MYSQLHOST'; 
+    $port = 'METTRE_ICI_LA_VALEUR_DE_MYSQLPORT';
+    $dbname = 'METTRE_ICI_LA_VALEUR_DE_MYSQLDATABASE';
+    $user = 'METTRE_ICI_LA_VALEUR_DE_MYSQLUSER';
+    $password = 'METTRE_ICI_LA_VALEUR_DE_MYSQLPASSWORD';
 
-    // Connexion dynamique
+    // Connexion forcée en TCP/IP avec les accès en clair
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
